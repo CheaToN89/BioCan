871ce13c0ee488e0e1beb9a0f21109a6a0136909
Juan edgar Alamo falcon <juanedgaralamofalcon@Mac-mini-de-Juan.local>
2026-08-11 21:13:02 +0100

    Crear primera pantalla de BioCan

--- App.tsx
+++ App.tsx
@@ -1,10 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Pressable } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.title}>BioCan</Text>
+
+      <Text style={styles.subtitle}>
+        Biodiversidad de Canarias
+      </Text>
+
+      <View style={styles.buttonsContainer}>
+        <Pressable style={styles.button}>
+          <Text style={styles.buttonIcon}>🐾</Text>
+          <Text style={styles.buttonText}>Fauna</Text>
+        </Pressable>
+
+        <Pressable style={styles.button}>
+          <Text style={styles.buttonIcon}>🌿</Text>
+          <Text style={styles.buttonText}>Flora</Text>
+        </Pressable>
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +30,44 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffffff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 20,
+  },
+
+  title: {
+    fontSize: 42,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+
+  subtitle: {
+    fontSize: 18,
+    marginBottom: 50,
+  },
+
+  buttonsContainer: {
+    flexDirection: 'row',
+    gap: 20,
+  },
+
+  button: {
+    width: 140,
+    height: 140,
+    borderRadius: 20,
+    backgroundColor: '#eeeeee',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  buttonIcon: {
+    fontSize: 45,
+    marginBottom: 10,
+  },
+
+  buttonText: {
+    fontSize: 22,
+    fontWeight: '600',
   },
 });
